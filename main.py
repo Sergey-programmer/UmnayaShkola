e5e67459a14871bbb735104064f37098f48ffc12
Sergey-programmer <dom-s-a@yandex.ru>
2025-11-08 17:58:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ import os
 from aiogram import Bot, Dispatcher, executor, types
 
 load_dotenv()
-bot = os.getenv("BOT_TOKEN")
+bot = Bot(os.getenv("BOT_TOKEN"))
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+  await message.answer("Всё работает!")
+
+executor.start_polling(dp)
